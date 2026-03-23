ba0afc1e6ddf00744362aa24325d84de4daceb88
HieuHo-hdh <hdh13300@gmail.com>
2026-03-24 01:12:34 +0700

    chore: update documents file, change log lib

--- backend/src/middleware/logger.ts
+++ backend/src/middleware/logger.ts
@@ -1,5 +1,5 @@
 import pino from 'pino';
-import expressPinoLogger from 'express-pino-logger';
+import pinoHttp from 'pino-http';
 
 export const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
@@ -9,4 +9,4 @@ export const logger = pino({
       : undefined,
 });
 
-export const requestLogger = expressPinoLogger({ logger: logger as any });
+export const requestLogger = pinoHttp({ logger });
